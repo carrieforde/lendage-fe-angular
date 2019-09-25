7e66438e349c8bc866414d2aa95b2a6215249389
Carrie Forde <cforde@bills.com>
2019-09-25 10:15:38 -0700

    Adds profile interface(s)

---
+++ src/app/profile.ts
@@ -0,0 +1,11 @@
+export interface Profile {
+  firstName: string;
+  lastName: string;
+  address?: string;
+  city?: string;
+  state?: string;
+  zip?: string;
+  phone?: string;
+  email?: string;
+  dob?: string;
+}

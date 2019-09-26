67329a4fc9b91243b8738b9c4753229af7a9fff5
Carrie Forde <cforde@bills.com>
2019-09-26 14:43:55 -0700

    Adds auth guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { ProfileDetailComponent } from './profile-detail/profile-detail.componen
 import { ProfileFormComponent } from './profile-form/profile-form.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { redirectUnauthorizedTo, AngularFireAuthGuard, redirectLoggedInTo } from '@angular/fire/auth-guard';
 
+const redirectUnauthorizedToLogin = redirectUnauthorizedTo(['login']);
+const redirectLogginInToItems = redirectLoggedInTo(['/']);
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'user/:id', component: ProfileDetailComponent },
+  { path: 'login', component: WelcomeComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectLogginInToItems } },
+  { path: 'user', component: UserListComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
+  { path: 'user/:id', component: ProfileDetailComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
   { path: 'user/:id/edit', component: ProfileFormComponent },
 ];
 
